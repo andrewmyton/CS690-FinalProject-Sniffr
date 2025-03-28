f9af49043f0d5068cc1dc79e3cf38ed6074f14fd
andrewmyton <andrew.myton@icloud.com>
2025-03-27 22:48:32 -0400

    updated HealthRecord class with a method

--- Sniffr/sniffr/HealthRecord.cs
+++ Sniffr/sniffr/HealthRecord.cs
@@ -6,6 +6,19 @@ public class HealthRecord
     public Dictionary<string, List<Datetime>> medicationAdministered = new Dictionary<string, List<Datetime>>();
     public List<DateTime> vetVisits = new List<DateTime>();
     public Dictionary<string,List<DateTime>> vaccinationRecords = new Dictionary<string, List<DateTime>>();
-    public 
+    
+
+
+
+    public void AddMedication(){
+        Console.Write("Enter medication name: ");
+        string medication = Console.ReadLine();
+
+        Console.Write("How often is this medication given (in days): ");
+        int interval = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter last administration date (MM/DD/YYYY): ")
+        
+    }
     
 }

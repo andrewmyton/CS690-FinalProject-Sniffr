8dde06fec21cbf5ca0c386b988d7c267f5af61e5
andrewmyton <andrew.myton@icloud.com>
2025-03-27 20:33:01 -0400

    edits to Pet and HealthRecord classes

---
+++ Sniffr/sniffr/HealthRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace sniffr;
+
+public class HealthRecord
+{
+    public Dictionary<string,int> medications = new Dictionary<string,int>();
+    public Dictionary<string, List<Datetime>> medicationAdministered = new Dictionary<string, List<Datetime>>();
+    public List<DateTime> vetVisits = new List<DateTime>();
+    public Dictionary<string,List<DateTime>> vaccinationRecords = new Dictionary<string, List<DateTime>>();
+    public 
+    
+}
---
+++ Sniffr/sniffr/Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace sniffr;
+
+public class Pet
+{
+    public string name;
+    public DateTime dateOfBirth;
+    
+    public HealthRecord healthRecord;
+
+    
+}

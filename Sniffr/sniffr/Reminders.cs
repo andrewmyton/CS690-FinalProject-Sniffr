f5b3e435647fcfc94da39ea91aff655baf9b200a
andrewmyton <andrew.myton@icloud.com>
2025-04-01 09:08:19 -0400

    added Reminder class

---
+++ Sniffr/sniffr/Reminders.cs
@@ -0,0 +1,19 @@
+﻿namespace sniffr;
+
+public class Reminders
+{
+    public List<string> reminders = new List<string>();
+
+    public static void AddReminder(){
+        Console.Write("Add Reminder: ")
+        string item = Console.ReadLine();
+        reminders.Add(item);
+
+    }
+
+    public static void RemoveReminder(string item){
+        reminders.Remove(item);
+    }
+
+
+}

7f7566150535a56d53bc4cfcb1fea1eb985096fc
andrewmyton <andrew.myton@icloud.com>
2025-04-08 10:59:20 -0400

    start program workflow

--- Sniffr/sniffr/Program.cs
+++ Sniffr/sniffr/Program.cs
@@ -1,28 +1,42 @@
 ﻿namespace sniffr;
 using System.Text.Json;
+using Spectre.Console;
 
 class Program
 {
+  
     static void Main(string[] args)
     {
-         PetManager petManager = new PetManager();
+        string[] mainMenu = {"Health", "Reminders"};
+        string[] healthChoices = {"Medication", "Vet Records", "Immunizations"};
 
-         if (File.ReadAllText("list-of-pets.txt").Length == 0){
-            petManager.AddPet();
-        }
+        //  PetManager petManager = new PetManager();
+
+        //  if (File.ReadAllText("list-of-pets.txt").Length == 0){
+        //     petManager.AddPet();
+        // }
         
-        // load a pet
-        string users = File.ReadAllText("list-of-pets.txt");
-        string[] userInfo = users.Split(":");
-        Pet currentPet = new Pet();
-        currentPet.uid = userInfo[0];
-        currentPet.name = userInfo[1];
-        currentPet.healthRecord = new HealthRecord();
+        // // load a pet
+        // string users = File.ReadAllText("list-of-pets.txt");
+        // string[] userInfo = users.Split(":");
+        // Pet currentPet = new Pet();
+        // currentPet.uid = userInfo[0];
+        // currentPet.name = userInfo[1];
+        // currentPet.healthRecord = new HealthRecord();
+        
+        
+        var menuChoices = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("How would like to manage your pet: ")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to select choice)[/]")
+            .AddChoices(mainMenu));
         
 
-
+    
+    
     }
 
-
+  
 
 }
